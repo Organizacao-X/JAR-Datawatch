c4052a3a8b09cd91b9808fcbce6c410f3504fbfb
Matheu5Leone <matheus.lsilva@sptech.school>
2023-04-12 13:02:27 -0300

    instanciado classe mac

--- datawatch/src/main/java/sptech/datawatch/Mac.java
+++ datawatch/src/main/java/sptech/datawatch/Mac.java
@@ -4,24 +4,24 @@ import java.net.*;
 import java.util.*;
 
 public class Mac {
-    public static void main(String[] args) {
+
+    public String getMac() {
         try {
             Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
             while (interfaces.hasMoreElements()) {
                 NetworkInterface iface = interfaces.nextElement();
                 byte[] mac = iface.getHardwareAddress();
                 if (mac != null) {
-                    System.out.print("Endereço MAC: ");
+                    StringBuilder sb = new StringBuilder();
                     for (int i = 0; i < mac.length; i++) {
-                        System.out.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : "");
+                        sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
                     }
-                    System.out.println();
+                    return sb.toString();
                 }
             }
         } catch (SocketException e) {
             System.out.println("Erro ao obter o endereço MAC: " + e.getMessage());
         }
+        return null;
     }
 }
-
-
