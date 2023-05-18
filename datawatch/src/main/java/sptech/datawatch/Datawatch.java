8a9807a7459bfb53141a2f7fbb94a49b9bf3c541
10340314 <victor.diaz@sptech.school>
2023-05-18 16:20:59 -0300

    alterações para docker funcionar

--- datawatch/src/main/java/sptech/datawatch/Datawatch.java
+++ datawatch/src/main/java/sptech/datawatch/Datawatch.java
@@ -15,11 +15,14 @@ import inserts.Inserts;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import tabelas.Usuarios;
 import java.time.Instant;
 import java.util.List;
+import java.util.Scanner;
 import javax.swing.Timer;
+import login.Sha2_256;
 import login.TelaLogin;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import selects.Selects;
@@ -29,20 +32,74 @@ import tabelas.Maquinas;
 public class Datawatch {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        Sha2_256 hash = new Sha2_256();
         Looca looca = new Looca();
         Conexao conexaoAzure = new Conexao("azure");
         Conexao conexaoMysql = new Conexao("mysql");
         JdbcTemplate jdbcAzure = conexaoAzure.getConnection();
         JdbcTemplate jdbcMysql = conexaoMysql.getConnection();
+        Scanner scnr = new Scanner(System.in);
+        Boolean autenticado = false;
+        Integer fkEmpresa = 0;
 
-        while (true) {
+        // VERIFICAÇÃO DA EXISTÊNCIA DESSE USUÁRIO NO BANCO
+        while (!autenticado) {
+            // LOGIN
+            System.out.println("Digite seu login");
+            String email = scnr.nextLine();
+            System.out.println("Digite sua senha");
+            String senha = scnr.nextLine();
+            if (email != null && senha != null) {
+                try {
+                    senha = hash.criarHash(senha);
+                } catch (NoSuchAlgorithmException ex) {
+                    System.out.println("Erro");
+                }
+
+                List<Usuarios> listaUsuarios = jdbcAzure.query("SELECT * FROM Usuarios WHERE email = ? AND senha = ?;", new BeanPropertyRowMapper(Usuarios.class), email, senha);
+
+                if (!listaUsuarios.isEmpty()) {
+                    System.out.println("Login realizado com sucesso! Conectando...");
+                    System.out.println(listaUsuarios.get(0).getEmail());
+                    fkEmpresa = listaUsuarios.get(0).getFkEmpresa();
+                    autenticado = true;
+                } else {
+                    System.out.println("USUÁRIO E/OU SENHA INVÁLIDO!");
+                }
+            }
+        }
+
+        // SE REALIZAR O LOGIN COM SUCESSO
+        if (autenticado) {
+            // VERIFICA SE ESSA MÁQUINA EXISTE NO BANCO DE DADOS
             Maquinas maquina = Selects.pegarMaquinaCorrespondente(jdbcAzure);
 
-            inserts.Inserts.capturarInserirDados(20, 1, jdbcAzure, jdbcMysql);
-            if (Selects.reebotar(jdbcAzure, 20, 1)) {
-                RebootOld.rebootar();
+            if (maquina == null) {
+                System.out.println("Máquina não existe. Inserindo dados no banco de dados");
+                // INSERE OS DADOS ESTÁTICOS DA MÁQUINA NO BD CASO NÃO EXISTA
+                inserts.Inserts.inserirDadosMaquinaEstatico(jdbcAzure, jdbcMysql, looca, fkEmpresa, null, false);
+            } else {
+                System.out.println("Máquina já existe\n");
+                Integer idMaquina = maquina.getIdMaquina();
+                // UPDATE NOS DADOS ESTÁTICOS DA MÁQUINA NO BD CASO EXISTA
+                inserts.Inserts.inserirDadosMaquinaEstatico(jdbcAzure, jdbcMysql, looca, null, idMaquina, true);
             }
-            Thread.sleep(5000);
+
+            // LOOP PARA CAPTURAR DADOS VOLÁTEIS
+            while (true) {
+                // RETORNA OS DADOS DO BD DESTA MÁQUINA
+                maquina = Selects.pegarMaquinaCorrespondente(jdbcAzure);
+
+                // INSERE UMA CAPTURA PARA OS DADOS VOLÁTEIS DESTA MÁQUINA
+                inserts.Inserts.capturarInserirDados(maquina.getIdMaquina(), maquina.getFkEmpresa(), jdbcAzure, jdbcMysql);
+                
+                // VERIFICA SE O COMANDO PARA REBOOTAR A MÁQUINA FOI ATIVADO
+                if (Selects.reebotar(jdbcAzure, maquina.getIdMaquina(), maquina.getFkEmpresa())) {
+                    RebootOld.rebootar();
+                }
+                // DELAY DE 5s PARA CONTINUAR O LAÇO DE REPETIÇÃO
+                Thread.sleep(5000);
+            }
         }
 
         // verificando se a máquina já existe no banco de dados através do endereço MAC
