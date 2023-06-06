f9875727fd009f6a295f20a0958e5252036426ec
10340314 <victor.diaz@sptech.school>
2023-06-06 10:46:46 -0300

    Slack alertas componentes

--- datawatch/src/main/java/config/slack/SlackEnumController.java
+++ datawatch/src/main/java/config/slack/SlackEnumController.java
@@ -8,16 +8,16 @@ import java.util.List;
 
 public class SlackEnumController {
 
-        private static LocalDateTime dataHoraAtual = LocalDateTime.now();
-        private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        private static String dataHoraFormatada = dataHoraAtual.format(formatter);
-        private static List<RedeInterface> ri = new Looca().getRede().getGrupoDeInterfaces().getInterfaces();
-        private static String ip = ri.get(ri.size() - 1).getEnderecoIpv4().get(0);
-                
-        private static String mensagem = null;
-    
+    private static LocalDateTime dataHoraAtual = LocalDateTime.now();
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    private static String dataHoraFormatada = dataHoraAtual.format(formatter);
+    private static List<RedeInterface> ri = new Looca().getRede().getGrupoDeInterfaces().getInterfaces();
+    private static String ip = ri.get(ri.size() - 1).getEnderecoIpv4().get(0);
+    private static String nomeMaquina = new Looca().getRede().getParametros().getHostName();
+    private static String mensagem = null;
+
     public static String logMessage(SlackEnum level) {
-        
+
 //        LocalDateTime dataHoraAtual = LocalDateTime.now();
 //        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 //        String dataHoraFormatada = dataHoraAtual.format(formatter);
@@ -25,13 +25,12 @@ public class SlackEnumController {
 //        String ip = ri.get(ri.size() - 1).getEnderecoIpv4().get(0);
 //                
 //        String mensagem = null;
-        
         switch (level) {
             case INFO_LOGIN:
-                mensagem = String.format(" :large_blue_circle: *[INFO]*  [%s]\t\t_IP [%s] conectou-se_", dataHoraFormatada, ip);
-                break; 
+                mensagem = String.format(" :large_blue_circle: *[INFO]*  [%s]\t\t_IP [%s] Máquina [%s] conectou-se_", dataHoraFormatada, ip, nomeMaquina);
+                break;
             case WARNING_LOGIN_FALHO:
-                mensagem = String.format(" :large_yellow_circle: *[WARNING]*  [%s]\t\t_IP [%s] TENTOU-SE CONECTAR_", dataHoraFormatada, ip);
+                mensagem = String.format(" :large_yellow_circle: *[WARNING]*  [%s]\t\t_IP [%s] Máquina [%s] TENTOU SE CONECTAR_", dataHoraFormatada, ip, nomeMaquina);
                 break;
             case WARNING:
                 mensagem = String.format(" :large_yellow_circle: *[WARNING]*  [%s]\t\t_IP tentou", dataHoraFormatada);
@@ -40,18 +39,59 @@ public class SlackEnumController {
                 mensagem = String.format(" :red_circle: *[ERROR]*  [%s]\t\t", dataHoraFormatada);
                 break;
             case DEBBUG:
-                mensagem = String.format(" :large_green_circle: *[DEBBUG]*  [%s]\t\t", dataHoraFormatada);;
+                mensagem = String.format(" :large_green_circle: *[DEBBUG]*  [%s]\t\t", dataHoraFormatada);
+                ;
                 break;
             default:
                 mensagem = ("[UNKNOWN] ");
                 break;
-                
+
         }
-                return mensagem;
+        return mensagem;
     }
-    
-    public static String slackAlertUso(SlackEnum level, String nomeComponente) {
-        mensagem = String.format(" :red_circle: *[WARNING]* O componente [%s] ultrapassou o limite estipulado\t\t", nomeComponente);
+
+    public static String slackAlertUso(Integer fkAlerta, String nomeComponente, String nomeMaquina) {
+        switch (fkAlerta) {
+            case 1:
+                mensagem = String.format(" :red_circle: *[WARNING]* Dados do processador [%s] não estão sendo capturados na máquina [%s]\t\t", nomeComponente, nomeMaquina);
+                break;
+            case 2:
+                mensagem = String.format(" :red_circle: *[WARNING]* O consumo de ram ultrapassou o limite estipulado por muito tempo na máquina [%s]\t\t", nomeMaquina);
+                break;
+            case 3:
+                mensagem = String.format(" :red_circle: *[WARNING]* O disco [%s] está com 90%% de espaço usado na máquina [%s]\t\t", nomeComponente, nomeMaquina);
+                break;
+            case 4:
+                mensagem = String.format(" :red_circle: *[WARNING]* Há um gap no envio de dados na máquina [%s]\t\t", nomeMaquina);
+                break;
+            case 5:
+                mensagem = String.format(" :red_circle: *[WARNING]* O disco [%s] está com 98%% de espaço usado na máquina [%s]\t\t", nomeComponente, nomeMaquina);
+                break;
+            case 6:
+                mensagem = String.format(" :red_circle: *[WARNING]* Dados do disco [%s] não estão sendo capturados na máquina [%s]\t\t", nomeComponente, nomeMaquina);
+                break;
+            case 7:
+                mensagem = String.format(" :red_circle: *[WARNING]* Dados da RAM não estão sendo capturados na máquina [%s]\t\t", nomeMaquina);
+                break;
+            case 8:
+                mensagem = String.format(" :red_circle: *[WARNING]* Dados de rede não estão sendo capturados na máquina [%s]\t\t", nomeMaquina);
+                break;
+            case 9:
+                mensagem = String.format(" :red_circle: *[WARNING]* O uso de ram está como nulo na máquina [%s]\t\t", nomeMaquina);
+                break;
+            case 10:
+                mensagem = String.format(" :red_circle: *[WARNING]* Uso do processador [%s] ultrapassou o limite estipulado na máquina [%s]\t\t", nomeComponente, nomeMaquina);
+                break;
+            case 11:
+                mensagem = String.format(" :red_circle: *[WARNING]* Uso da RAM ultrapassou o limite estipulado na máquina\t\t", nomeMaquina);
+                break;
+            case 12:
+                mensagem = String.format(" :red_circle: *[WARNING]* O uso do disco [%s] ultrapassou o limite estipulado na máquina [%s]\t\t", nomeComponente, nomeMaquina);
+                break;
+            default:
+                mensagem = ("[UNKNOWN]");
+                break;
+        }
         return mensagem;
     }
 }
--- datawatch/src/main/java/service/LogService.java
+++ datawatch/src/main/java/service/LogService.java
@@ -4,11 +4,15 @@
  */
 package service;
 
+import config.slack.ConexaoSlack;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import model.Capturas;
 import model.Maquinas;
+import org.json.JSONObject;
+import static config.slack.SlackEnumController.slackAlertUso;
+import java.io.IOException;
 import repository.LogRepository;
 
 /**
@@ -19,8 +23,9 @@ public class LogService {
 
     private static final LogRepository logRepository = new LogRepository();
     private static Integer contador = 0;
+    private static JSONObject json = new JSONObject();
 
-    public static void criarAlerta(Maquinas maquina) {
+    public static void criarAlerta(Maquinas maquina) throws IOException, InterruptedException {
         Integer fkMaquina = maquina.getIdMaquina();
         Integer fkEmpresa = maquina.getFkEmpresa();
         Capturas captura = CapturasService.getUltimaCaptura(fkMaquina);
@@ -34,17 +39,20 @@ public class LogService {
             // insert tabela alerta dado CPU não capturado
             value = String.format("(1, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
             queryValues.add(value);
+            ConexaoSlack.sendMessage(json.put("text", slackAlertUso(1, maquina.getProcessador(), maquina.getNomeMaquina())));
         }
 
         if (captura.getCpuUso() >= maquina.getCpuMetrica()) {
             value = String.format("(10, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
             queryValues.add(value);
+            ConexaoSlack.sendMessage(json.put("text", slackAlertUso(10, maquina.getProcessador(), maquina.getNomeMaquina())));
         }
 
         if (captura.getRamUso() == null) {
             // insert tabela alerta dado RAM não capturado
             value = String.format("(9, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
             queryValues.add(value);
+            ConexaoSlack.sendMessage(json.put("text", slackAlertUso(9, null, maquina.getNomeMaquina())));
         }
 
         if (captura.getRamUso() >= maquina.getRamMetrica()) {
@@ -52,6 +60,7 @@ public class LogService {
             if (contador == 10) {
                 value = String.format("(11, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
                 queryValues.add(value);
+                ConexaoSlack.sendMessage(json.put("text", slackAlertUso(11, null, maquina.getNomeMaquina())));
                 contador = 0;
             } else {
             }
@@ -60,41 +69,49 @@ public class LogService {
         if (captura.getLivreDisco1() == null) {
             value = String.format("(6, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
             queryValues.add(value);
+            ConexaoSlack.sendMessage(json.put("text", slackAlertUso(6, maquina.getNomeDisco1(), maquina.getNomeMaquina())));
         }
 
         if (maquina.getTotalDisco1() - captura.getLivreDisco1() >= maquina.getGatilhoDisco1()) {
             value = String.format("(12, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
             queryValues.add(value);
+            ConexaoSlack.sendMessage(json.put("text", slackAlertUso(12, maquina.getNomeDisco1(), maquina.getNomeMaquina())));
         }
 
         if (maquina.getTotalDisco1() - captura.getLivreDisco1() >= (maquina.getTotalDisco1() * 0.9)) {
             value = String.format("(3, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
             queryValues.add(value);
+            ConexaoSlack.sendMessage(json.put("text", slackAlertUso(3, maquina.getNomeDisco1(), maquina.getNomeMaquina())));
         }
 
         if (maquina.getTotalDisco1() - captura.getLivreDisco1() >= (maquina.getTotalDisco1() * 0.98)) {
             value = String.format("(5, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
             queryValues.add(value);
+            ConexaoSlack.sendMessage(json.put("text", slackAlertUso(5, maquina.getNomeDisco1(), maquina.getNomeMaquina())));
         }
 
         if (maquina.getNomeDisco2() != null) {
             if (captura.getLivreDisco2() == null) {
                 value = String.format("(6, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
                 queryValues.add(value);
+                ConexaoSlack.sendMessage(json.put("text", slackAlertUso(6, maquina.getNomeDisco2(), maquina.getNomeMaquina())));
             }
 
             if (maquina.getTotalDisco2() - captura.getLivreDisco2() >= maquina.getGatilhoDisco2()) {
                 value = String.format("(12, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
                 queryValues.add(value);
+                ConexaoSlack.sendMessage(json.put("text", slackAlertUso(12, maquina.getNomeDisco2(), maquina.getNomeMaquina())));
             }
 
             if (maquina.getTotalDisco2() - captura.getLivreDisco2() >= (maquina.getTotalDisco2() * 0.9)) {
                 value = String.format("(3, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
                 queryValues.add(value);
+                ConexaoSlack.sendMessage(json.put("text", slackAlertUso(3, maquina.getNomeDisco2(), maquina.getNomeMaquina())));
             }
             if (maquina.getTotalDisco2() - captura.getLivreDisco2() >= (maquina.getTotalDisco2() * 0.98)) {
                 value = String.format("(5, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
                 queryValues.add(value);
+                ConexaoSlack.sendMessage(json.put("text", slackAlertUso(5, maquina.getNomeDisco2(), maquina.getNomeMaquina())));
             }
         }
 
@@ -102,27 +119,32 @@ public class LogService {
             if (captura.getLivreDisco3() == null) {
                 value = String.format("(6, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
                 queryValues.add(value);
+                ConexaoSlack.sendMessage(json.put("text", slackAlertUso(6, maquina.getNomeDisco3(), maquina.getNomeMaquina())));
             }
 
             if (maquina.getTotalDisco3() - captura.getLivreDisco3() >= maquina.getGatilhoDisco3()) {
                 value = String.format("(12, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
                 queryValues.add(value);
+                ConexaoSlack.sendMessage(json.put("text", slackAlertUso(12, maquina.getNomeDisco3(), maquina.getNomeMaquina())));
             }
 
             if (maquina.getTotalDisco3() - captura.getLivreDisco3() >= (maquina.getTotalDisco3() * 0.9)) {
                 value = String.format("(3, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
                 queryValues.add(value);
+                ConexaoSlack.sendMessage(json.put("text", slackAlertUso(3, maquina.getNomeDisco3(), maquina.getNomeMaquina())));
             }
 
             if (maquina.getTotalDisco3() - captura.getLivreDisco3() >= (maquina.getTotalDisco3() * 0.98)) {
                 value = String.format("(5, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
                 queryValues.add(value);
+                ConexaoSlack.sendMessage(json.put("text", slackAlertUso(5, maquina.getNomeDisco3(), maquina.getNomeMaquina())));
             }
         }
 
         if (captura.getRedeDownload() == null && captura.getRedeUpload() == null) {
             value = String.format("(8, %d, %d, '%s')", fkMaquina, fkEmpresa, dataHora);
             queryValues.add(value);
+            ConexaoSlack.sendMessage(json.put("text", slackAlertUso(8, null, maquina.getNomeMaquina())));
         }
 
         String values = queryValues.toString().replaceAll("\\[|\\]", "");
